873920cb1d62058e8d3a9e23c40e8f40afdad8d8
ECortus <ve.cortus@gmail.com>
2026-09-24 22:49:43 +0200

    upd hud

---
+++ Source/Ascentetik/ProjectSaveGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "ProjectSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASCENTETIK_API UProjectSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
