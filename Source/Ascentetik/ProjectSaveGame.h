54da472bb37b42670b6118c7675810292e5cbc42
ECortus <ve.cortus@gmail.com>
2026-09-26 00:22:10 +0200

    fixing code loading

--- Source/Ascentetik/ProjectSaveGame.h
+++ Source/Ascentetik/ProjectSaveGame.h
@@ -9,9 +9,31 @@
 /**
  * 
  */
-UCLASS()
+
+USTRUCT(BlueprintType)
+struct FGameSaveFields
+{
+	GENERATED_BODY()
+};
+
+USTRUCT(BlueprintType)
+struct FSettingsSaveFields
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MasterVolume;
+};
+
+UCLASS(BlueprintType)
 class ASCENTETIK_API UProjectSaveGame : public USaveGame
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameSaveFields Game;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FSettingsSaveFields Settings;
 };
